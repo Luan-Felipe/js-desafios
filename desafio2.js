e0fc09f1c928b0d128c2fb67980014fce07c2f61
Luan Felipe <luanfel2009@gmail.com>
2023-05-31 17:11:05 -0300

    Finalização  desafio 2

--- desafio2.js
+++ desafio2.js
@@ -1,1 +1,12 @@
+/*
+    2- Faça um programa que receba N (quantidade de numeros) e seus respectivos valores
+    Imprima o maior numero par e o maior numero impar.
+
+    Exemplo:
+        Entrada: 5 3 4 1 10 8
+        Saida:  maior numero par 10
+                maior numero impar 3
+*/
 const { gets, print } = require('./funcoes-auxiliares-ex2');
+
+print(gets());
--- funcoes-auxiliares-ex2.js
+++ funcoes-auxiliares-ex2.js
@@ -1,10 +1,27 @@
+const entradas = [5, 3, 4, 1, 10, 8];
+let i = 0;
+let menorImpar = null;
+let maiorPar = null;
+
 function gets() {
-    return 10;
+  while (i < entradas.length - 1) {
+    let valor = entradas[i + 1];
+    if (valor % 2 === 0) {
+      if (maiorPar === null || maiorPar < valor) {
+        maiorPar = valor;
+      }
+    } else {
+      if (menorImpar === null || valor < menorImpar) {
+        menorImpar = valor;
+      }
+    }
+    i++;
   }
-  
-  function print(texto) {
-    console.log(texto);
-  }
-  
-  module.exports = { gets, print };
-  
+  return `Maior numero par: ${maiorPar}\nMenor numero impar: ${menorImpar}`;
+}
+
+function print(texto) {
+  console.log(texto);
+}
+
+module.exports = { gets, print };
