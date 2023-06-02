f82eab6192912952b8221163d5636c868bef0e57
Luan-Felipe <luanfel2009@gmail.com>
2023-06-01 21:35:10 -0300

    entrega do desafio 3

--- desafio3.js
+++ desafio3.js
@@ -1,1 +1,24 @@
+/*
+    3- Faça um programa que calcule e imprima o salário a ser transferido para um funcionário.
+    Para realizar o calculo receba o valor bruto do salário e o adicional dos benefícios,
+    o salário a ser tranferido é calculado da seguinte maneira:
+
+    valor bruto do salário - percentual do imposto mediante faixa salarial + adicional dos benefícios.
+
+    Para calcular o percentual de impostos segue as aliquotas:
+
+        De R$0.00 a R$1100.00 = 5.00%
+        De R$1100.00 a R$2500.00 = 10.00%
+        Mais que R$2500.00 = 15.00%
+
+        Exemplo:
+            Entrada:
+                2000
+                250
+
+            Saída:
+                2050.00
+*/
 const { gets, print } = require('./funcoes-auxiliares-ex3');
+
+print(gets());
--- funcoes-auxiliares-ex3.js
+++ funcoes-auxiliares-ex3.js
@@ -1,10 +1,22 @@
+const salarioBruto = 2501;
+const beneficios = 250;
+const aliquotas = [0.05, 0.1, 0.15];
+
 function gets() {
-    return 10;
+  let salarioLiquido = 0;
+  if (salarioBruto >= 0 && salarioBruto <= 1100) {
+    salarioLiquido = salarioBruto - (salarioBruto * aliquotas[0]) + beneficios;
+  } else if (salarioBruto > 1000.01 && salarioBruto <= 2500) {
+    salarioLiquido = salarioBruto - (salarioBruto * aliquotas[1]) + beneficios;
+  } else {
+    salarioLiquido = salarioBruto - (salarioBruto * aliquotas[2]) + beneficios;
   }
-  
-  function print(texto) {
-    console.log(texto);
-  }
-  
-  module.exports = { gets, print };
-  
+
+  return salarioLiquido.toFixed(2);
+}
+
+function print(texto) {
+  console.log(texto);
+}
+
+module.exports = { gets, print };
